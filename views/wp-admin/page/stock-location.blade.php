cdf60940ba41e1c8d6663940710a9825793ee360
Glynn Quelch <glynn.quelch@gmail.com>
2022-07-15 14:46:24 +0100

    House Keeping

--- views/wp-admin/page/stock-location.blade.php
+++ views/wp-admin/page/stock-location.blade.php
@@ -1,4 +1,4 @@
-  <script type="module" crossorigin src="https://pinkcrab.gq/wp-content/plugins/stock-managment/build/js/assets/index.06b10c46.js"></script>
+  <script type="module" crossorigin src="https://pinkcrab.gq/wp-content/plugins/stock-managment/build/js/assets/index.1b9fa8a3.js"></script>
   <link rel="stylesheet" href="https://pinkcrab.gq/wp-content/plugins/stock-managment/build/js/assets/index.91f7a831.css">
 
 <h1>{{ $i18n->location_page_title() }}</h1>
