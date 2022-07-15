cf94be5707fa2bd7417f33e7962393750ed18b20
Glynn Quelch <glynn.quelch@gmail.com>
2022-07-15 15:02:28 +0100

    House Keeping

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/production-sub-path/' :
+        '/'
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
